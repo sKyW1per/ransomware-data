cd1c8ffe4e43cead15a74f995031043bc525bc66
sKyw1per <47339295+linatl@users.noreply.github.com>
2022-07-20 00:36:53 +0200

    finished voorbeeld_2.py

--- voorbeeld_2.py
+++ voorbeeld_2.py
@@ -14,15 +14,46 @@ def get_dataset2():
 
     return mydata
 
+def count_values(blob):
+    years = {}
+    for v in blob["values"]:
+        if "meta" in v:
+            for m in v["meta"]:
+                if "date" in m:
+                    date_unchanged = v["meta"]["date"]
+                    date_splitted = str.split(date_unchanged)
+                    t = date_splitted[1].isnumeric()
+                    y = [e for e in date_splitted if e.isnumeric()]
+                    y_int = int(y[0])
+                    if y_int not in years:
+                        years[y_int] = 0
+                    years[y_int] += 1
+    return years
 
 def main():
     #run your code here
 
     #parse csv -> dictionary
     blob = get_dataset2()
+    years = count_values(blob)
+    years_sorted = sorted(years)
+    print(years)
+    # make graph with matplotlib
 
-    #So far, this just prints all data to terminal output.
-    print(json.dumps(blob, indent=2))
+    titles = list(years_sorted)
+    values = []
+    for y in sorted(years):
+        print(y)
+        values.append(years[y])
+
+    plt.plot(titles, values)
+    plt.ylabel('Aantal waarnemingen per jaar')
+
+    # for testing: show the graph
+#    plt.show()
+
+    # print the graph to a png and store the png in the "output" folder
+    plt.savefig('output/graph_2.png')
 
     print("\ndone!")
 
